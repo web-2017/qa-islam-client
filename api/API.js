7e657ef205f71f70f196401fede86325649974be
Mario Magomedov <webdevelope2017@gmail.com>
2021-10-03 19:38:31 -0500

    update env

--- api/API.js
+++ api/API.js
@@ -1,10 +1,4 @@
 import {Platform} from "react-native";
 
-// export const BASE_URL = process.env.NODE_ENV === 'development' && Platform.OS === 'ios' ? 'http://localhost:5000' : 'http://10.0.2.2:5000'
 export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://qa-islam.herokuapp.com'
 // export const BASE_URL_ANDROID = 'http://10.0.2.2:5000'
-// console.log(process.env.NODE_ENV)
-
-
-
-
