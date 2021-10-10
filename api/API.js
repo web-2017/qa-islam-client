bf40aebe46fabf04fd922e0bda93082d53c3d23a
Mario Magomedov <webdevelope2017@gmail.com>
2021-10-10 17:44:14 -0500

    update latest libs version package.json

--- api/API.js
+++ api/API.js
@@ -1,4 +1,5 @@
 import {Platform} from "react-native";
 
 export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://qa-islam.herokuapp.com'
+// export const BASE_URL = 'https://qa-islam.herokuapp.com'
 // export const BASE_URL_ANDROID = 'http://10.0.2.2:5000'
