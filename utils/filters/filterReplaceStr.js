df8f46fe0650b97196fa2142f3f90f2abdffc0dc
Mario Magomedov <webdevelope2017@gmail.com>
2021-10-20 20:25:34 -0500

    Доавил filter для 'коран', 'Коран

--- utils/filters/filterReplaceStr.js
+++ utils/filters/filterReplaceStr.js
@@ -1,3 +1,3 @@
 export const filterReplaceStr = str => {
-    return str ? str && str.replace('Мухаммад', 'Мухаммад ﷺ') : null
+    return str ? str && str.replace('Мухаммад', 'Мухаммад ﷺ').replace('коран', 'Коран') : null
 }
